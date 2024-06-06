a2f74b333f4db434754f702aa60e9e314e455fc3
harsh khandelwal <harshkhandelwal756@gmail.com>
2024-06-06 18:23:05 +0530

    two input method

---
+++ two_inputs_method.py
@@ -0,0 +1,9 @@
+# name,age=input("enter your age and name ").split()
+# print(name)
+# print(age)
+# name,age=input("enter your age and name ").split(",")
+# print(name)
+# print(age)
+name="harsh"
+age=98
+print(f"your name is {name} and your age is {age}") 
