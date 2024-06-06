5dd27f031bb7231f500ee61556c361cc152893af
harsh khandelwal <harshkhandelwal756@gmail.com>
2024-06-06 19:01:16 +0530

    add string strip  method

---
+++ string_method.py
@@ -0,0 +1,8 @@
+name="   ha  rsh     "
+dots="__________"
+# print(name+dots)
+# print(name.lstrip()+dots)
+# print(name.rstrip()+dots)
+# print(name.strip()+dots)
+print(name.replace(" ",""))
+# print(name)
