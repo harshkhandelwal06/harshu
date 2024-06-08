5a263cb7d14bb951f8079061a4b0bd70e2fc32c3
harsh khandelwal <harshkhandelwal756@gmail.com>
2024-06-08 14:04:25 +0530

    check and in statement is complete

---
+++ check_empty_or_not.py
@@ -0,0 +1,5 @@
+name=input("enter your name: ")
+if name:
+    print(f"your name is {name} ")
+else:
+    print("your not enter your name")
---
+++ in_keyword.py
@@ -0,0 +1,5 @@
+name="harsh"
+if 'a' in name:
+    print("a is present in name")
+else:
+    print("a is not present in name")
