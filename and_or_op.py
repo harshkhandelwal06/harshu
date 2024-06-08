a85eb8ecfb862373e4c332bee4e83988cd539f49
harsh khandelwal <harshkhandelwal756@gmail.com>
2024-06-08 13:04:22 +0530

    and or operator compete

---
+++ and_or_op.py
@@ -0,0 +1,6 @@
+age=int(input("enter your age :"))
+name=input("enter your name :")
+if name=="harsh" and age >=18:
+    print(f"you are {name} and your  age is {age}")
+else:
+     print("your name is not correct or you are not ginus" )
