b0ea64b0a18a705d24e1b747e98f24f947933c05
harsh khandelwal <harshkhandelwal756@gmail.com>
2024-06-10 08:32:20 +0530

    task complete

---
+++ default.py
@@ -0,0 +1,7 @@
+def show(first_name,last_name,age=20):
+    print(f"your first name is : {first_name}")
+    print(f"your last name is : {last_name}")
+    print(f"your age name is : {age}")
+
+show("harsh","khandelwal" )
+    
---
+++ function.py
@@ -0,0 +1,9 @@
+# def add_two(a,b):
+#     print(a+b)
+
+# c=int(input("enter a first number: "))
+# d=int(input("enter a second  number: "))
+# add_two(c,d)
+name="harsh"
+for i in "mohit":
+    print(i ,end="")
