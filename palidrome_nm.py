360023580584d9e78adedf1b9253ab1b7d925bfb
harsh khandelwal <harshkhandelwal756@gmail.com>
2024-06-10 14:36:54 +0530

    palidrome complete

---
+++ palidrome_nm.py
@@ -0,0 +1,10 @@
+# name=input("enter the name : ")
+def check(name):
+    checking = ''.join(reversed(name))
+    if name.lower()==checking.lower():
+        print("this is palidrome")
+    else:
+        print("this word not a palidrome ")
+
+name=input("enter the name : ")
+check(name)
