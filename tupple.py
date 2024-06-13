e3b4c19f8793253518966f8bcd48438de839376e
harsh khandelwal <harshkhandelwal756@gmail.com>
2024-06-13 17:44:32 +0530

    tuple complete

---
+++ tupple.py
@@ -0,0 +1,29 @@
+mixed=(1,2,3,4.5)
+# # # print(mixed)
+# # # print(type(mixed))
+# # # # mixed[1]=6
+# # # print(mixed[1])
+# # for i in mixed:
+# #     print(i,end=" ")
+# # num=(2)
+# # print(num,end=" ")
+# # print(type(num))
+# # tuple without parenthesis
+# name="harsh","ganesh","vinay"
+# print(name)
+# print(type(name))
+# tupple unpacking
+# name=("harsh","ganesh","vinay",[23,54,65],["aadipurush","creature 3D"])
+# name1,name2,name3=name
+# print(name1)
+# print(name2)
+# print(name3)
+# name[3].pop()
+# name[3].append("raja")
+# print(name)
+# print(max(mixed))
+# print(min(mixed))
+# print(sum(mixed))
+nums=list((1,2,3,4,5,6))
+print(nums)
+print(type(nums))
