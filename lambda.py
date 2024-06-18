43482a9ba5bc07a2a1fe6bf9849419bc833cc41b
harsh khandelwal <harshkhandelwal756@gmail.com>
2024-06-18 17:55:26 +0530

    maping complete

--- lambda.py
+++ lambda.py
@@ -4,6 +4,7 @@
 # add=lambda a,b:a+b
 # print(add(6,6))
 # finding=lambda a :a%2==0
-# print(finding(6))
-for pos , name in enumerate(name):
-    print(f"{pop}-->{name}")
+# # print(finding(6))
+# for pos,name in enumerate (name):
+#     print(f"{pos}-->{name}")
+
